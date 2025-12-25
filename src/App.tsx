e24ccf164d8d7c6789c95034ba0181542e878f39
Filipe Reges de Salles <filipe.salles@seatecnologia.com.br>
2025-12-25 18:15:29 -0300

    fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import * as stylex from "@stylexjs/stylex";
-import { globalStyles } from "./styles/globalStyles";
 
 function App() {
   return (
-    <main {...stylex.props(globalStyles.body)}>
+    <main>
       <Outlet />
     </main>
   );
--- src/styles/errorPageStyles.ts
+++ src/styles/errorPageStyles.ts
@@ -1,34 +1,7 @@
 import * as stylex from "@stylexjs/stylex";
-import { colors, fonts, borderRadius, shadows } from "./theme.stylex";
 
 const styles = stylex.create({
-  container: {
-    backgroundColor: colors.backgroundLight,
-    color: colors.textMain,
-    fontFamily: fonts.body,
-  },
-
-  containerDark: {
-    backgroundColor: colors.backgroundDark,
-    color: colors.textLight,
-  },
-
-  primaryButton: {
-    backgroundColor: colors.primary,
-    color: colors.textLight,
-    borderRadius: borderRadius.default,
-    boxShadow: shadows.soft,
-  },
-
-  card: {
-    backgroundColor: colors.cardLight,
-    borderRadius: borderRadius.default,
-    boxShadow: shadows.soft,
-  },
-
-  cardDark: {
-    backgroundColor: colors.cardDark,
-  },
+  
 });
 
 export default styles;

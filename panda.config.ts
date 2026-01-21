c9efdca063e6ec6bcfbb03460a8b7e67cc903618
Filipe Reges de Salles <filipe.salles@seatecnologia.com.br>
2026-01-21 19:18:09 -0300

    panda-css instalation

---
+++ panda.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "@pandacss/dev";
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+
+  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+
+  exclude: [],
+
+  theme: {
+    extend: {},
+  },
+
+  outdir: "styled-system",
+});
